971e97d47fda990376f7f5773b4265d1a0972f71
N <34268416+ziloen@users.noreply.github.com>
2023-04-03 16:28:07 +0800

    feat: global storage change event

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -1,3 +1,29 @@
+import { useSubscription } from '@vueuse/rxjs'
+import { fromEventPattern, share } from 'rxjs'
+import { Storage as ExtensionStorage } from 'webextension-polyfill'
 import { useStorageLocal } from '~/composables/useStorageLocal'
 
+
+
 export const storageDemo = useStorageLocal('webext-demo', 'Storage Demo')
+
+
+
+export const $storageLocalChange =
+  fromEventPattern<ExtensionStorage.StorageAreaOnChangedChangesType>(
+    handler => browser.storage.local.onChanged.addListener(handler),
+    handler => browser.storage.local.onChanged.removeListener(handler)
+  ).pipe(
+    share({ resetOnRefCountZero: true })
+  )
+
+
+
+export function useStorageLocalChange(key: string, onChange: (change: ExtensionStorage.StorageChange) => void) {
+  useSubscription(
+    $storageLocalChange.subscribe(changes => {
+      const target = changes[key]
+      target && onChange(target)
+    })
+  )
+}
