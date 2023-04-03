4706bd58b194b66f1e7725aa97bc8810f49fdbef
N <34268416+ziloen@users.noreply.github.com>
2023-04-03 17:34:08 +0800

    chore: add docs

--- src/logic/storage.ts
+++ src/logic/storage.ts
@@ -9,6 +9,9 @@ export const storageDemo = useStorageLocal('webext-demo', 'Storage Demo')
 
 
 
+/**
+ * browser.storage.local.onChanged 事件流
+ */
 export const $storageLocalChange =
   fromEventPattern<ExtensionStorage.StorageAreaOnChangedChangesType>(
     handler => browser.storage.local.onChanged.addListener(handler),
@@ -19,6 +22,18 @@ export const $storageLocalChange =
 
 
 
+/**
+ * 监听 browser.storage.local 的变化
+ * @param key 要监听的 key
+ * @param onChange 变化时的回调
+ * @example
+ * ```ts
+ * useStorageLocalChange('key', ({ newValue, oldValue }) => {
+ *   console.log("new value:", newValue)
+ *   console.log("old value:", oldValue)
+ * })
+ * ```
+ */
 export function useStorageLocalChange(key: string, onChange: (change: ExtensionStorage.StorageChange) => void) {
   useSubscription(
     $storageLocalChange.subscribe(changes => {
