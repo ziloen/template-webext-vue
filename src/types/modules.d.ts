c25b8f755d9183fa95111e19e6f1c1eeeb016bdf
N <34268416+ziloen@users.noreply.github.com>
2023-12-09 18:16:47 +0800

    chore: remove outdated comments

--- src/types/modules.d.ts
+++ src/types/modules.d.ts
@@ -1,5 +1,3 @@
-// import type { ProtocolWithReturn } from 'webext-bridge'
-
 declare module '@vue/runtime-core' {
   interface ComponentCustomProperties {
     $app: {
@@ -8,15 +6,5 @@ declare module '@vue/runtime-core' {
   }
 }
 
-// declare module 'webext-bridge' {
-//   export interface ProtocolMap {
-//     // define message protocol types
-//     // see https://github.com/antfu/webext-bridge#type-safe-protocols
-//     'tab-prev': { title: string | undefined }
-//     'get-current-tab': ProtocolWithReturn<{ tabId: number }, { title?: string | undefined }>
-//   }
-// }
-
 // https://stackoverflow.com/a/64189046/479957
-export { }
-
+export {}
