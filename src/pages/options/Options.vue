a8369028b0b0bde8f998c6891f64da5de76f20d8
N <34268416+ziloen@users.noreply.github.com>
2023-12-12 18:34:05 +0800

    chore: update class

--- src/pages/options/Options.vue
+++ src/pages/options/Options.vue
@@ -6,11 +6,11 @@ import iconSrc from "~/assets/icon.svg"
 
 <template>
   <main class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
-    <img :src="iconSrc" class="icon-btn mx-2 text-2xl" alt="extension icon">
-    <div >Options</div>
+    <img :src="iconSrc" class="icon-btn mx-2 text-2xl" alt="extension icon" />
+    <div>Options</div>
     <SharedSubtitle />
 
-    <input v-model="storageDemo" class="border border-gray-400 rounded px-2 py-1 mt-2">
+    <input v-model="storageDemo" class="border border-gray-400 rounded px-2 py-1 mt-2" />
 
     <div class="mt-4 bg-red text-blue">
       Powered by Vite <pixelarticons-zap class="align-middle inline-block" />
